273a3dafe3b24f46a13f614d7f88fc816f069163
Islam Madkaur <islammadkaur@gmail.com>
2020-10-09 20:47:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,6 +8,11 @@ end
 
 def update_counting_hash(hash, key)
   { key => "hello"}
+  if key == nil{
+    { key: 1}
+  }
+
+  end
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
